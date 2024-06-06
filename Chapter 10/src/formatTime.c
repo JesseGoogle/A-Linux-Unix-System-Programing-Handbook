9d34fc5373fdfd6e952f615a0a777e797d309aaf
JesseGoogle <zhj3191955858@gmail.com>
2024-06-06 23:04:30 +0800

    把一个 tm 结构体的数据转换成一个表示时间信息的字符串

---
+++ Chapter 10/src/formatTime.c
@@ -0,0 +1,23 @@
+#include <sys/unistd.h>
+
+#include <time.h>
+
+#include "../../include/tlpi_hdr.h"
+#include "../include/timeFuncAssistant.h"
+
+int main(int argc, char const *argv[])
+{
+    time_t curTimeStamp = time(NULL);
+    struct tm * localTimeInfo = localtime(&curTimeStamp);
+
+    /**
+     * asctime() 函数用于把一个 tm 结构体的数据转换成一个表示时间信息的字符串,
+     * 格式和 ctime() 一样
+    */
+    char * localTimeString = asctime(localTimeInfo);
+
+    printf("%s", localTimeString);
+    showtmStruct(localTimeInfo);
+
+    return EXIT_SUCCESS;
+}
