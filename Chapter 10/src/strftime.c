3e4735c61cf5b96a03048309c59bb1b618bbc0f0
JesseGoogle <zhj3191955858@gmail.com>
2024-06-07 20:10:29 +0800

    strftime() 函数的用法

--- Chapter 10/src/strftime.c
+++ Chapter 10/src/strftime.c
@@ -1,12 +1,63 @@
+#define _XOPEN_SOURCE
+
 #include <sys/unistd.h>
 
 #include "../include/timeFuncAssistant.h"
 
-int main(int argc, char const *argv[])
+#include <locale.h>
+
+#define TS_BUFFER_SIZE 1024
+
+char * getProgramName(char * __programPath);
+
+int main(int argc, char * argv[])
 {
     system("clear");
 
-    printf("%s\n", getCurrentTime("%F %T"));
+    struct tm timeMessage = {0};
+    char timeStringBuff[TS_BUFFER_SIZE] = {0};
+    char * outputFormat = NULL;
+
+
+    if (argc < 3 || strcmp(argv[1], "--help") == 0)
+    {
+        usageErr("[%s] <input-date-time> <in-format> [out-format]\n", getProgramName(argv[0]));
+    }
+
+    /**
+     * 设置本地化环境, LC_ALL 宏意味着影响所有本地化类别
+    */
+    if (!setlocale(LC_ALL, "")) { errExit("setlocale()"); }
+
+    if (!strptime(argv[1], argv[2], &timeMessage)) { fatal("strptime()"); }
+
+    timeMessage.tm_isdst = -1;
+
+    printf("Current time stamp (seconds since Epoch) = %ld\n", (long int)mktime(&timeMessage));
+
+    outputFormat = (argc > 3) ? argv[3] : "%H:%M:%S:%A, %d %B %Y %Z";
+
+    if (!strftime(timeStringBuff, TS_BUFFER_SIZE, outputFormat, &timeMessage))
+    {
+        fatal("strftime() return 0.\n");
+    }
+
+    printf("strftime() yields: %s\n", timeStringBuff);
     
     return EXIT_SUCCESS;
+}
+
+char * getProgramName(char * __programPath)
+{
+    if (!__programPath || !(*__programPath)) 
+    { 
+        fatal("getProgramName(): NULL or empty programPath.\n"); 
+    }
+
+    size_t pathLen = strlen(__programPath);
+    char * pathEnd = __programPath + pathLen;
+
+    while ((*pathEnd) != '/') { --pathEnd; }
+
+    return (pathEnd + 1);
 }
